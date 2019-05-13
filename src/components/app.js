615e25d36cac28739340c90f6a56d1b30d83f3ce
SyedHTanveer <Syedt21@gmail.com>
2019-05-13 16:37:38 -0400

    added a landing-page component, a nav compoenent, and firebase auth setup.

--- src/components/app.js
+++ src/components/app.js
@@ -1,60 +1,22 @@
-import React from 'react';
+import '../style.scss';
 import {
-  BrowserRouter as Router, Route, NavLink, Switch,
+  BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
+import React from 'react';
+import LandingPage from './components/landing-page';
 
-import Counter from '../containers/counter';
-import Controls from '../containers/controls';
-
-const Nav = (props) => {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
-  );
-};
-
-const About = (props) => {
-  return <div> All there is to know about me </div>;
-};
-
-const Welcome = (props) => {
-  return (
-    <div>
-      <p>Welcome</p>
-      <Counter />
-      <Controls />
-    </div>
-  );
-};
-
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
-};
-
-const FallBack = (props) => {
-  return <div>URL Not Found</div>;
-};
-
-const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/test/:id" component={Test} />
-          <Route component={FallBack} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route render={() => (<div>post not found </div>)} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import * as firebase from 'firebase';
 import reducers from './reducers';
-
 import App from './components/app';
 import './style.scss';
 
@@ -13,6 +13,20 @@ const store = createStore(reducers, {}, compose(
   applyMiddleware(),
   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
 ));
+
+const FireBaseConfig = {
+  apiKey: process.env.firebase_apiKey,
+  authDomain: process.env.firebase_authDomain,
+  databaseURL: process.env.firebase_databaseURL,
+  projectId: process.env.firebase_projectId,
+  storageBucket: process.env.firebase_storageBucket,
+  messagingSenderId: process.env.firebase_messagingSenderId,
+};
+
+// initialize firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(FireBaseConfig);
+}
 
 // we now wrap App in a Provider
 ReactDOM.render(
