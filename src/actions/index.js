d6f1303a0610b8f4ba57c53272455a132b13e72d
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-05-15 17:32:12 -0400

    connected component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,35 @@
 // keys for actiontypes
+import axios from 'axios';
+
+const LOCAL_URL = 'http://hocalhost:9090/api';
+
 export const ActionTypes = {
-  INCREMENT: 'INCREMENT',
-  DECREMENT: 'DECREMENT',
+  UPLOAD_FILE: 'UPLOAD_FILE',
 };
 
+export function uploadFile(selectedFile) {
+  return (dispatch) => {
+    console.log('------------');
+    console.log(selectedFile);
 
-export function increment() {
-  return {
-    type: ActionTypes.INCREMENT,
-    payload: null,
-  };
-}
+    const fd = new FormData();
+    fd.append('fileTest', selectedFile);
 
-export function decrement() {
-  return {
-    type: ActionTypes.DECREMENT,
-    payload: null,
+    axios.post(`${LOCAL_URL}/upload`, fd, {
+      headers: {
+        authorization: localStorage.getItem('token'),
+        'Content-Type': 'multipart/form-data',
+      },
+      withCredentials: true,
+      'Access-Control-Allow-Credentials': true,
+    }).then((response) => {
+      // now update contact with newly synced
+      dispatch({ type: ActionTypes.UPLOAD_FILE });
+      console.log('Maybe didn\'t get an error woo?');
+      console.log(response);
+    }).catch((error) => {
+      console.log('Upload file error:');
+      console.log(error);
+    });
   };
 }
--- src/components/jsonTutorial.js
+++ src/components/jsonTutorial.js
@@ -1,28 +1,35 @@
-import React from 'react';
-// import React, { Component } from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import '../style.scss';
-import jsonScreen from '../img/jsonTutorialImage.png';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import UploadScreen from '../img/Drag_and_Drop_to_Upload.png';
+import uploadJSON from '../actions';
 
 
-// class JsonTutorial extends Component {
-export const JsonTutorial = () => {
-//   render() {
-  return (
-    <div className="whole-page">
-      <h1 id="JSON_File_Title">Upload Your JSON File</h1>
-      <h2><span id="step">Step 1:</span> Go to <a href="https://takeout.google.com/settings/takeout?pli=1">Google Takeout</a></h2>
-      <h2><span id="step">Step 2:</span> Sign In</h2>
-      <h2><span id="step">Step 3:</span> In  &quot;Select Data to Include&quot; only Select Location History.</h2>
-      <h3>If you don't have Location History - don't worry! Hit Done and Download Our App!</h3>
-      <h2><span id="step">Step 4:</span> Select  &quot;One Time Archive&quot;.</h2>
-      <h2><span id="step">Step 5:</span> Click on &quot;Create Archive&quot;.</h2>
-      {/* <img className="logo" src={jsonScreen} alt="" /> */}
-      <h2><span id="step">Step 6:</span> When You Receive Your Email, Open the Link and Download the File.</h2>
-      <div className="button_container">
-        <button type="submit" className="doneJSON">Done</button>
+class JsonTutorial extends Component {
+  onUploadClick = () => {
+    this.props.uploadFile();
+  }
+
+  render() {
+    return (
+      <div className="whole-page">
+        <h1 id="JSON_File_Title">Upload Your JSON File</h1>
+        <h2><span id="step">Step 1:</span> Go to <a href="https://takeout.google.com/settings/takeout?pli=1">Google Takeout</a></h2>
+        <h2><span id="step">Step 2:</span> Sign In</h2>
+        <h2><span id="step">Step 3:</span> In  &quot;Select Data to Include&quot; only Select Location History.</h2>
+        <h3>If you don&apos;t have Location History - don&apos;t worry! Hit Done and Download Our App!</h3>
+        <h2><span id="step">Step 4:</span> Select  &quot;One Time Archive&quot;.</h2>
+        <h2><span id="step">Step 5:</span> Click on &quot;Create Archive&quot;.</h2>
+        <h2><span id="step">Step 6:</span> When You Receive Your Email, Open the Link and Download the File.</h2>
+        <h2><span id="step">Step 7:</span></h2>
+        <img onClick={this.onUploadClick} className="UploadIMG" src={UploadScreen} alt="" />
+        <div className="button_container">
+          <button type="submit" className="doneJSON">Done</button>
+        </div>
       </div>
-    </div>
-  );
-//   }
-};
-export default JsonTutorial;
+    );
+  }
+}
+export default withRouter(connect(null, { uploadJSON })(JsonTutorial));
